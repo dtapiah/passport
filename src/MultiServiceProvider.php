8de630373d9dd7eb2bcbfe687921805099cdd555
Danilo <danilo.tapiah@gmail.com>
2018-01-03 01:00:39 -0300

    asdasd

--- src/MultiServiceProvider.php
+++ src/MultiServiceProvider.php
@@ -3,6 +3,7 @@
 namespace jumpitt\passport;
 
 use Illuminate\Support\ServiceProvider;
+use File;
 
 class MultiServiceProvider extends ServiceProvider
 {
@@ -14,8 +15,10 @@ class MultiServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
+            __DIR__.'/config/auth.php' => config_path('auth.php'),
             __DIR__.'/models' => app_path(),
-            __DIR__.'/middleware' => app_path(),
+            __DIR__.'/middleware' => app_path('Http/Middleware'),
+            __DIR__.'/migrations' => database_path('migrations'),
         ]);
     }
 
@@ -26,8 +29,16 @@ class MultiServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(
-            __DIR__.'/config/auth.php', 'auth.guards'
-        );
+        $auth = fopen(config_path('auth.php'), "w");
+        if($auth == True) {
+            $bytesWritten = File::append(config_path('auth.php'), 
+                File::get(__DIR__.'/config/auth.php'));
+        }
+
+        $authProvider = fopen(app_path('Providers/AuthServiceProvider.php'), "w");
+        if($auth == True) {
+            $bytesWritten = File::append(capp_path('Providers/AuthServiceProvider.php'), 
+                File::get(__DIR__.'/config/AuthServiceProvider.php'));
+        }
     }
 }
